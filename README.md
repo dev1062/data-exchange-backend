5e1cc47fd35944f9c20a5d20a8128250c1319d89
Jacob Cooley <j.cooley42@gmail.com>
2019-12-19 16:43:15 -0700

    Updated readme

---
+++ README.md
@@ -0,0 +1,6 @@
+### Data-Exchange
+
+install the dependencies by running 
+`yarn`
+
+Next, start the dev server by running `yarn dev`
--- src/server.test.js
+++ src/server.test.js
@@ -1,9 +1,36 @@
+const moxios = require('moxios')
+const request = require('supertest');
+const express = require('express');
+
+const initApp = () => {
+  const app = express();
+  return app;
+}
+
 describe('GET Endpoints', () => {
+  beforeEach(function() {
+    moxios.install()
+  })
+
+  afterEach(function() {
+    moxios.uninstall()
+  })
+
   it('should create a new get request', async () => {
-    const res = await request(app)
-      .get('/pairs/binance')
-      .send()
-    expect(res.statusCode).toEqual(201)
-    expect(res.body).toHaveProperty('get')
+    moxios.stubRequest(/exchanges/, {
+      status: 200,
+      response: [
+        { id: 20, symbol: 'luno', name: 'Luno', route: 'https://api.cryptowat.ch/exchanges/luno', active: true },
+        { id: 21, symbol: 'poloniex', name: 'Poloniex', route: 'https://api.cryptowat.ch/exchanges/poloniex', active: true },
+        { id: 2, symbol: 'coinbase-pro', name: 'Coinbase Pro', route: 'https://api.cryptowat.ch/exchanges/coinbase-pro', active: true },
+      ],
+    })
+    const app = initApp();
+    await request(app).get('/exchanges');
+    // const res = await request(app)
+    //   .get('/pairs/binance')
+    //   .send()
+    // expect(res.statusCode).toEqual(201)
+    // expect(res.body).toHaveProperty('get')
   })
 })
